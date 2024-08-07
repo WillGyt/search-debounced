39b198b99f6307f63fa56392e097e0a26f9741a2
WillGoYeah <william.goyet@gmail.com>
2024-08-07 20:55:45 +0200

    adding local key and url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,8 @@ export default defineNuxtConfig({
 
   supabase: {
     redirect: false,
+    url: 'http://127.0.0.1:54321',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
   },
 
   ssr: false,
